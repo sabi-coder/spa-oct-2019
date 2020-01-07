3e8fec1c0b5af274f4fe45b703455ae681ccac28
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-06 19:42:00 -0600

    :construction: add link for JS.

---
+++ components/index.js
@@ -0,0 +1,8 @@
+/**
+ * Export whatever comes out of Header.js as "Header".
+ * All of this will be exported out as an ED Module with the given keys.
+ */
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as Main } from "./Main";
+export { default as Nav } from "./Nav";
