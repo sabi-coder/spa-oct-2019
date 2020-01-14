f33e13085b0b47376ae0353837e68c25271d4f35
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-13 19:57:54 -0600

    :recycle: Destructure and add template literals

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-// Destructure' each of the named components from the components directory.
+// 'Destructure' each of the named components from the components directory.
 import { Header, Nav, Main, Footer } from "./components";
 
 import capitalize from "lodash.capitalize";
 
-const state = {
-  Home: {
-    heading: "Home"
-  },
-  Form: {
-    heading: "Form"
-  },
-  Blog: {
-    heading: "Blog"
-  },
-  Gallery: {
-    heading: "Gallery"
-  }
-  Links: ["Home", "Blog", "Gallery", "Contact"]
-};
+import * as state from "./store";
 
 function render(st = state.Home) {
-  document.querySelector("#root").innerHTML = `
-  ${Header(st)}
-  ${Nav(st)}
-  ${Main(st)}
-  ${Footer(st)}
-  `;
+  document.querySelector("#root").innerHTML = `${Header(st)} ${Nav(
+    state.Links
+  )} ${Main(st)} ${Footer(st)}`;
+  render();
 }
-
-render();
-// TODO: Listen for clicks on our menu and log what was clicked on.
+//TODO: Listen for clicks on our menu and log what was clicked on.
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", function() {
     event.preventDefault();
-    console.log(event.target.textContent);
-
-    // TODO: Use this information and log out the correct piece of state.
-    let navText = event.target.textContent;
-    render(state[capitalize(navText)]);
+    render(state[capitalize(event.target.textContent)]);
   });
 });
