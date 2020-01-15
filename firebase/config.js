58eff46bc61c96f9bac4ac78589c5f8af8325115
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-14 19:03:52 -0600

    Hid API key

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+import "../env.js";
+
+export default {
+  apiKey: `${process.env.FIREBASE_API}`,
+  authDomain: "savvy-coders-practice.firebaseapp.com",
+  databaseURL: "https://savvy-coders-practice.firebaseio.com",
+  projectId: "savvy-coders-practice",
+  storageBucket: "savvy-coders-practice.appspot.com",
+  messagingSenderId: "14926798271",
+  appId: "1:14926798271:web:497ed2870baead8582550a"
+};
